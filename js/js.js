df68643ba26edfa6270c729876e2100cf8b43ea7
aliona-ivanova1 <ivanovaaliona42@gmail.com>
2025-03-14 09:28:12 +0400

    ss

---
+++ js/js.js
@@ -0,0 +1,9 @@
+// burger bar
+
+let navbar = document.getElementById('navbar')
+let burgerbar = document.getElementById('burgerbar')
+
+burgerbar.addEventListener('click',function(){
+    navbar.classList.toggle('newnavigation')
+})// dacheraze ixsneba nav
+
